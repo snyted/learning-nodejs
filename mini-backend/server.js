f7b87a90e4dd402f57b29f31f77386c9a4d4be1d
João Vitor Farias <joaofarias_16@hotmail.com>
2025-10-13 12:23:22 -0300

    test on postman method post working

--- mini-backend/server.js
+++ mini-backend/server.js
@@ -1,25 +1,42 @@
 const http = require("http");
 
-const veiculos = [
+const vehicle = [
   { marca: "Toyota", modelo: "Corolla", ano: 2020 },
   { marca: "Honda", modelo: "Civic", ano: 2021 },
 ];
 
 const server = http.createServer((req, res) => {
+  // Método GET
   if (req.method === "GET" && req.url === "/veiculos") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(veiculos));
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(vehicle));
     return;
   }
 
+  // Método POST
   if (req.method === "POST" && req.url === "/veiculos") {
     let body = "";
 
-    req.on("data", (chunk) => console.log(chunk.toString()));
+    req.on("data", (chunk) => {
+      console.log("Chunk:", chunk);
 
-    req.on("end", () => {
-      res.end("Terminou");
+      body += chunk.toString();
+      console.log("novo veiculo: ", body);
     });
+    req.on("end", () => {
+      try {
+        const newVehicle = JSON.parse(body);
+        vehicle.push(newVehicle);
+
+        res.writeHead(201, { "Content-Type": "application/json" });
+        res.end(body);
+      } catch (error) {
+        console.error("Erro:", error);
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end("JSON inválido no corpo da requisição.");
+      }
+    });
+
     return;
   }
 

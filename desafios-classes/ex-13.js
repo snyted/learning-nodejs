aa4e3a948f483dcbdd1d1631c1b4b4f6b8aa9d46
snyted <joaofarias_16@hotmail.com>
2026-05-26 14:13:06 -0300

    Add Calculadora class with static sum and mult methods

---
+++ desafios-classes/ex-13.js
@@ -0,0 +1,19 @@
+class Calculadora {
+    // constructor(a, b) {
+    //     this.a = a;
+    //     this.b = b
+    // }
+
+    static sum(a, b) {
+        return a + b
+    }
+
+    static mult(a, b) {
+        return a * b
+    }
+}
+
+
+
+console.log(Calculadora.sum(2, 5))
+console.log(Calculadora.mult(2, 5))

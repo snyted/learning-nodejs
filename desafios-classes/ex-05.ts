10520cfecb4e07338ef74612db3fc5bbbf31cfe1
snyted <joaofarias_16@hotmail.com>
2026-05-29 10:46:07 -0300

    Refactor: migrate Product class to TypeScript; implement formatted price method

--- desafios-classes/ex-05.ts
+++ desafios-classes/ex-05.ts
@@ -1,10 +1,8 @@
 class Product {
-  constructor(name, price) {
-    this.name = name;
-    this.price = price;
+  constructor(public name: string, public price: number) {
   }
 
-  get formatedPrice() {
+  get formatedPrice(): string {
     return `R$ ${this.price.toFixed(2)}`;
   }
 }
@@ -16,15 +14,15 @@ console.log(compra1.formatedPrice);
 console.log(compra2.formatedPrice);
 
 // Esse aqui é um outro exemplo
-class Product2 {
-  get formatedPrice() {
-    return `R$ ${this.price.toFixed(2)}`;
-  }
-}
+// class Product2 {
+//   get formatedPrice(): string {
+//     return `R$ ${this.price.toFixed(2)}`;
+//   }
+// }
 // De acordo com o estudo, atribuir um valor de forma manual, e não quando for instanciar
 // um objeto, é uma prática ruim, então o primeiro exemplo segue sendo o certo.
-let testCompra1 = new Product2();
-testCompra1.name = "Shampoo";
-testCompra1.price = 30.3;
+// let testCompra1 = new Product2();
+// testCompra1.name = "Shampoo";
+// testCompra1.price = 30.3;
 
-console.log(testCompra1);
+// console.log(testCompra1);

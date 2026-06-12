1f1bb139d2755923e4ebdbaba4fb3470477d4e4e
snyted <joaofarias_16@hotmail.com>
2026-06-12 12:47:08 -0300

    Refactor: update boolean.ts with additional examples and console logs

--- typescript/boolean.ts
+++ typescript/boolean.ts
@@ -2,3 +2,16 @@ let nomeVariavel: boolean = true;
 // Boolean !== boolean
 // Boolean -> tipo Object
 // boolean -> tipo primitivo
+
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log(tarefaConcluida, tarefaPendente)
+
+let concluido: boolean = false;
+
+if (!concluido) {
+    console.log('Tarefa concluida com sucesso!')
+} else {
+    console.log('Tarefa Pendente!')
+}

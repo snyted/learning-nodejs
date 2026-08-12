1b57fe257891865cfc8ce13b516acb1e09ef4691
snyted <joaofarias_16@hotmail.com>
2026-08-12 09:20:54 -0300

    Add cart schema validation with Zod and example usage

--- zod/index.ts
+++ zod/index.ts
@@ -22,3 +22,45 @@ const user = {
 const { success, error } = userSchema.safeParse(user)
 
 console.log(success, error?.message[0])
+
+// 03
+
+const cartSchema = z.object({
+    clientId: z.uuid(),
+    products: z.array(
+        z.object({
+            name: z.string().min(3),
+            price: z.number().min(1),
+            qnt: z.number().min(1),
+            categories: z.array(z.string().min(1))
+        })
+    )
+})
+
+const cart = {
+    clientId: crypto.randomUUID(),
+    products: [
+        {
+            name: "Macbook",
+            price: 5000,
+            qnt: 1,
+            categories: ["Eletronico", "Computador"]
+        }
+    ]
+}
+
+console.log(cartSchema.safeParse(cart))
+
+const cart02 = {
+    clientId: crypto.randomUUID(),
+    products: [
+        {
+            name: "Mouse",
+            price: 0,
+            qnt: 1,
+            categories: ["Eletronico", "Computador", "Periferico"]
+        }
+    ]
+}
+
+console.log("Schema Cart 02: ", cartSchema.safeParse(cart02).error?.message)
